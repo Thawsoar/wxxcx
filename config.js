a0e99915f819a4a0ce420c62e04cc6c0124ec397
thawsoar <1306750238@qq.com>
2018-12-19 01:28:21 +0800

    no message

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   appkey: 'he6u813YZjtMkyWI',
   api_base_url: 'http://bl.7yue.pro/v1/',
-  err: '此程序为体验版，若无数据，请尝试点击右上角打开调试面板'
+  err: '此程序为体验版，若无数据，请尝试点击右上角打开调试面板！'
 }
 export {config}
